7daf7e45f80e850168d900f0a43fce8920f04ad0
bezborodov-2010 <bezborodov_2010@mail.ru>
2023-11-22 16:24:43 +0700

    do 6 part of hw2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println("Задача 2");
         System.out.println("dog = "+dog);
         System.out.println("cat = "+cat);
         System.out.println("paper = "+paper);
@@ -10,18 +12,21 @@ public class Main {
         var newCat = cat+4;
         var newPaper = paper+4;
         System.out.println("new dog = "+newDog+" new cat = "+newCat+" new paper = "+newPaper);
+        System.out.println("Задача 3");
         var dog2 = newDog-3.5;
         var cat2 = newCat-1.6;
         var paper2 = newPaper-7639;
         System.out.println("dog "+dog2);
         System.out.println("cat "+ cat2);
         System.out.println("paper "+paper2);
+        System.out.println("Задача 4");
         var friend = 19;
         System.out.println("friend = "+friend);
         var friend2 = friend+2;
         System.out.println("friend = "+friend2);
         var friend3 = friend2/7;
         System.out.println("friend = "+friend3);
+        System.out.println("Задача 5");
         var frog = 3.5;
         System.out.println("frog = "+frog);
         var frog2 = frog*10;
@@ -30,5 +35,12 @@ public class Main {
         System.out.println("frog = "+frog3);
         var frog4 = frog3+4;
         System.out.println("frog = "+frog4);
+        System.out.println("Задача 6");
+        var weightBoxer1 = 78.2;
+        var weightBoxer2 = 82.7;
+        var result = weightBoxer1+weightBoxer2;
+        System.out.println("общий вес боксеров "+result+" кг");
+        var result2 = weightBoxer2-weightBoxer1;
+        System.out.println("разница в весе боксеров "+result2+" кг");
     }
 }
